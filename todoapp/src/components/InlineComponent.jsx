18d1d0929d15db22afea98bba4eacfae0ddf9b96
Shubham Samarth <shubhams@heaptrace.net>
2024-06-03 11:09:19 +0530

    create and style header

--- todoapp/src/components/InlineComponent.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./inlinecomponent.module.css";
-export default function InlineComponent() {
-  return (
-    <>
-      <h1 className={styles.header}>This is an Inline Component</h1>
-    </>
-  );
-}
