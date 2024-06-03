f38794ededa854189247cf970f63718883b194f8
Shubham Samarth <shubhams@heaptrace.net>
2024-06-03 11:43:09 +0530

    style the form

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,3 +1,4 @@
+import styles from "./form.module.css";
 import { useState } from "react";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
@@ -8,13 +9,19 @@ export default function Form({ todos, setTodos }) {
     setTodo("");
   }
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoform} action="" onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.modernInput}
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter ToDo Item"
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
