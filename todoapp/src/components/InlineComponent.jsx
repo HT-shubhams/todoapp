a5e60cc04d6847aa86f6ea5d4df0356e63ab3d79
Shubham Samarth <shubhams@heaptrace.net>
2024-06-03 10:42:25 +0530

    css stylesheet

--- todoapp/src/components/InlineComponent.jsx
+++ todoapp/src/components/InlineComponent.jsx
@@ -1,13 +1,8 @@
-const header = { color: "blue", fontSize: "140px" }; // Way 2
-
+import "../css/style.css";
 export default function InlineComponent() {
   return (
     <>
-      {/* Way 1 */}
-      {/* //inline Styling // outermoist brackets are for JS code and inner brackets are for defining an object with CSS properties // Properties are in camelCase and are in JSX */}
-      <h1 style={{ color: "violet", fontSize: "140px" }}>Way 1</h1>
-      {/* Way 2 */}
-      <h1 style={header}>Way 2</h1>
+      <h1 className="header">CSS Stylesheets</h1>
     </>
   );
 }
